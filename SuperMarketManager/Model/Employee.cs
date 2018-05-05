a8254cb33143e7755e485b4bfa5ad52b98f3ce9a
新宇 李 <1039795376@qq.com>
2018-05-05 17:01:52 +0800

    创建数据库表模型

---
+++ SuperMarketManager/Model/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperMarketManager.Model
+{
+    public class Employee
+    {
+        public const int SEX_MAN = 0;
+        public const int SEX_WOMAN = 1;
+
+        public int Id { set; get; }
+        public String Name { set; get; }
+        public String Phone { set; get; }
+        public int Sex { set; get; }
+        public int PartId { set; get; }
+        public long Time { set; get; }
+    }
+}
