eea7a57f15b6acc3b5e388bb166a10e91a3bd4c2
杨志坤 <1071526708@qq.com>
2018-05-06 17:35:30 +0800

    登录信息验证

---
+++ SuperMarketManager/Service/LoginService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperMarketManager.Service
+{
+    public class LoginService
+    {
+        //验证管理员身份登录密码
+        public bool identifyManager(string name,string pswd)
+        {
+            if (name.Trim() != "admin" || pswd.Trim() != "admin")
+                return false;
+            else
+                return true;
+        }
+    }
+}
