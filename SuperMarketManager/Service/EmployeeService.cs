032109f03c854642442f26087574b7fa53248a20
杨志坤 <1071526708@qq.com>
2018-05-12 15:25:19 +0800

    员工表，增加了根据部门Id和员工姓名查询员工信息的方法。

--- SuperMarketManager/Service/EmployeeService.cs
+++ SuperMarketManager/Service/EmployeeService.cs
@@ -11,9 +11,9 @@ namespace SuperMarketManager.Service
     public class EmployeeService
     {
         private const String INSERT_EMPLOYEE_SQL = "insert into employee(name,phone,sex,part_id,time) values(\"{0}\",\"{1}\",{2},{3},{4},{5})";
-        private const String SELECT_EMPLOYEE_BY_PART = "select * from employee were part_id={0}";
+        private const String SELECT_EMPLOYEE_BY_PART = "select * from employee where part_id={0}";
         private const String SELECT_EMPLOYEE_BY_ID = "select * from employee where id={0}";
-
+        private const String SELECT_EMPLOYEE_BY_PART_NAME = "select * from employee where part_id={0} and name={1}";
 
         public bool AddEmployee(Employee employee)
         {
@@ -52,6 +52,24 @@ namespace SuperMarketManager.Service
             else
             {
                 return Employee.CreateEmployee(result[0]);
+            }
+        }
+
+        public List<Employee> GetEmployeeByPartName(String partId, String name)
+        {
+            List<Dictionary<String, Object>> result = DatabaseTool.ExecSqlWithReturn(String.Format(SELECT_EMPLOYEE_BY_PART_NAME, partId,name));
+            if (null == result || result.Count < 1)
+            {
+                return null;
+            }
+            else
+            {
+                List<Employee> employees = new List<Employee>();
+                foreach (Dictionary<String, Object> dic in result)
+                {
+                    employees.Add(Employee.CreateEmployee(dic));
+                }
+                return employees;
             }
         }
 
